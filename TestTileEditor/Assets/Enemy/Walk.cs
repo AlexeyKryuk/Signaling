23aa76147de8ae89cccbb508f850419d96b3ecbf
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2020-07-29 16:49:11 +0400

    Спаун врагов
    Добавление спауна врагов

---
+++ TestTileEditor/Assets/Enemy/Walk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float obstacleRange = 3.0f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+
+        Ray ray = new Ray(transform.position, transform.forward * 2f);
+        RaycastHit hit;
+
+        if (Physics.SphereCast(ray, 1f, out hit))
+        {
+            if (hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+
+    }
+}
