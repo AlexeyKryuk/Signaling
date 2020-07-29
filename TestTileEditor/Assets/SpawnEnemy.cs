9d8ec0e52aa8f2404d0b2e5cbdc4d4a020c8bb96
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2020-07-29 13:55:37 +0400

    Добавление проекта

---
+++ TestTileEditor/Assets/SpawnEnemy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPoints;
+
+}
